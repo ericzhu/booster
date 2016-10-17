96e4914563ae54536b02525d2441d08499f6f97e
Eric Zhu <eric.zhu@esignlive.com>
2016-10-17 15:44:09 -0400

    init commit

---
+++ booster_bootstrap/booster_bootstrap_autoconfiguration/src/main/java/com/booster/BoosterBootstrapAutoconfigApplication.java
@@ -0,0 +1,13 @@
+package com.booster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoosterBootstrapAutoconfigApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(BoosterBootstrapAutoconfigApplication.class, args);
+	}
+
+}
---
+++ booster_bootstrap/booster_bootstrap_maven/src/main/java/com/booster/BootstrapMavenApplication.java
@@ -0,0 +1,13 @@
+package com.booster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapMavenApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapMavenApplication.class, args);
+	}
+	
+}
